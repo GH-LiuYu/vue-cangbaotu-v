4cd5745c8981d728662ec8b900ffb8d2b9b04fa6
xhm8866 <979036781@qq.com>
2021-04-21 00:00:38 +0800

    ''

--- src/views/index/Main.vue
+++ src/views/index/Main.vue
@@ -4,36 +4,13 @@
         <Search></Search>
        <div class="list">
            <div class="card">
-                <el-col :span="8" class="cardli">
-                   <el-card shadow="always">
-                       小说
-                   </el-card>
-                </el-col>
-               <el-col :span="8" class="cardli">
-                   <el-card shadow="always">
-                       影视
-                   </el-card>
-               </el-col>
-               <el-col :span="8" class="cardli">
-                   <el-card shadow="always">
-                       书籍
-                   </el-card>
-               </el-col>
-               <el-col :span="8" class="cardli">
-                   <el-card shadow="always">
-                       游戏
-                   </el-card>
-               </el-col>
-               <el-col :span="8" class="cardli">
-                   <el-card shadow="always">
-                       短视频
-                   </el-card>
-               </el-col>
-               <el-col :span="8" class="cardli">
-                   <el-card shadow="always">
-                       网站访问热度排行1234
-                   </el-card>
-               </el-col>
+
+                   <el-col :span="4" class="cardli" @click.native="selectBlock(item.id)" v-for="(item,index) in list">
+                       <el-card shadow="always" >
+                           {{item.name}}
+                       </el-card>
+                   </el-col>
+
            </div>
 
        </div>
@@ -47,6 +24,14 @@
     data() {
       return {
         clientHeight:'',
+          list:[
+              {id:'1','name':'小说'},
+              {id:'2','name':'影视'},
+              {id:'3','name':'书籍'},
+              {id:'4','name':'文档'},
+              {id:'5','name':'短视频'},
+              {id:'6','name':'短视频'},
+          ],
       };
     },
     mounted(){
@@ -60,6 +45,9 @@
     },
     methods: {
 
+        selectBlock:function (id) {
+            console.log(id)
+        }
     },
   };
 </script>
@@ -69,18 +57,26 @@
         display: flex;
         justify-content:center;
         align-items:center;
-        width: 77%;
         margin: auto;
         padding: 10px;
+
     }
     .card{
         padding: 5px;
+        flex-basis: 80%;
+        display: flex;
+        flex-wrap:wrap;
     }
     .cardli{
-        padding: 5px;
+        padding: 11px;
+        flex-shrink:1;
     }
     .el-card {
-        height: 240px;
+        height: 168px;
+        /*flex-basis: 50%;*/
+    }
+    .el-card.is-always-shadow{
+        cursor: pointer;
     }
 </style>
 
