7685c25431db7d9f9f30757c6d9b14234c918b52
GH-LiuYu <979036781@qq.com>
2021-05-07 11:03:06 +0800

    ''

--- src/api/config/config.js
+++ src/api/config/config.js
@@ -1,10 +1,10 @@
 //调试切换
-const DEBUG = true;//true 本地服
-// const DEBUG = false;//false 正式服
+// const DEBUG = true;//true 本地服
+const DEBUG = false;//false 正式服
 //配置请求api 测试服务/正式服务器
 const APIURL = {
     local: "http://www.sanhuliangmeng.com/api",//本地测试
-    serve: "http://www.sanhuliangmeng.com/api",//正式服
+    serve: "http://192.168.186.128/api",//正式服
 };
 //返回服务地址
 export const baseUrl = DEBUG ? APIURL.local : APIURL.serve;
